1e0ac7006a76f7c5e9da1e11a7d4feaaac3be156
Narges Rameh <narges.rameh23@gmail.com>
2024-11-02 18:15:39 +0330

    Count Chart was created.

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,4 +1,5 @@
 import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart";
 
 const AdminPage = () => {
     return (
@@ -12,6 +13,20 @@ const AdminPage = () => {
                     <UserCard type='parents'/>
                     <UserCard type='staff'/>
                 </div>
+              {/*MIDDLE CHARTS*/}
+                <div className="flex gap-4 flex-col lg:flex-row">
+                    {/*COUNT CHART*/}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart />
+                    </div>
+                    {/*ATTENDANCE CHART*/}
+                    <div className="w-full lg:w-2/3 h-[450px]"></div>
+                </div>
+                {/*BOTTOM CHARTS*/}
+                <div className="">
+
+                </div>
+
             </div>
             {/*RIGHT*/}
             <div className="w-full lg:w-1/3 flex flex-col gap-8">r</div>
